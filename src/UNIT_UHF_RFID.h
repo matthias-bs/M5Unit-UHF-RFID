c00780dbe53d61d693e33f2065e44c9377b924df
Matthias Prinke <m.prinke@arcor.de>
2026-08-01 21:36:43 +0200

    Added get/setRxDemodParams(),  lockCard(), sleep()/wakeup(), refactored debug messages

--- src/UNIT_UHF_RFID.h
+++ src/UNIT_UHF_RFID.h
@@ -44,9 +44,15 @@ class Unit_UHF_RFID {
    public:
     void begin(HardwareSerial *serial = &Serial2, int baud = 115200, uint8_t RX = 16, uint8_t TX = 17,
                bool debug = false);
+    void debugFrame(const char *fn, size_t len = 25, bool is_cmd = false);
     String getVersion();
+    bool getVersion(String &version);
+    bool sleep();
+    void wakeup();
     bool getOperatingRegion(uint8_t &region);
     bool setOperatingRegion(uint8_t region);
+    bool getRxDemodParams(uint8_t &mixer_g, uint8_t &if_g, int16_t &thrd);
+    bool setRxDemodParams(uint8_t mixer_g, uint8_t if_g, int16_t thrd);
     String selectInfo();
     uint8_t pollingOnce();
     uint8_t pollingMultiple(uint16_t polling_count);
@@ -54,6 +60,7 @@ class Unit_UHF_RFID {
     bool setTxPower(uint16_t db);
     bool writeCard(uint8_t *data, size_t size, uint8_t membank, uint16_t sa, uint32_t access_password = 0);
     bool readCard(uint8_t *data, size_t size, uint8_t membank, uint16_t sa, uint32_t access_password = 0);
+    bool lockCard(uint32_t flags, uint32_t access_password);
 };
 
 #endif
