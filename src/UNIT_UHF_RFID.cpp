92abf10ae5d5f9481f781689f245afc267d4e330
Matthias Prinke <m.prinke@arcor.de>
2026-09-08 21:11:10 +0200

    Fix debugFrame function

--- src/UNIT_UHF_RFID.cpp
+++ src/UNIT_UHF_RFID.cpp
@@ -118,6 +118,7 @@ void Unit_UHF_RFID::debugFrame(const char *fn, size_t len, bool is_cmd)
         for (size_t i = 0; i < n; i++)
         {
             Serial.print(hex2str(buffer[i]));
+        }
         Serial.println();
         Serial.println("-------------------------");
     }
