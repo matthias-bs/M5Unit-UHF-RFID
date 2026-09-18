b257dd47fe2d8827572643c9f423a1d3fe7e957d
Matthias Prinke <m.prinke@arcor.de>
2026-09-18 19:01:39 +0200

    Add IDLE mode commands and methods for entering/exiting IDLE mode

--- src/UNIT_UHF_RFID.cpp
+++ src/UNIT_UHF_RFID.cpp
@@ -232,6 +232,45 @@ void Unit_UHF_RFID::wakeup()
     _serial->write(0xFF);
 }
 
+/*! @brief Enter manual IDLE mode. A zero-minute timer disables automatic entry. */
+bool Unit_UHF_RFID::enterIdle(uint8_t idleTimeMinutes)
+{
+    memcpy(buffer, ENTER_IDLE_CMD, sizeof(ENTER_IDLE_CMD));
+    buffer[7] = idleTimeMinutes;
+    buffer[8] = calculateChecksum(buffer, 1, 7);
+
+    debugFrame(__FUNCTION__, sizeof(ENTER_IDLE_CMD), true);
+    sendCMD(buffer, sizeof(ENTER_IDLE_CMD));
+    if (waitMsg())
+    {
+        debugFrame(__FUNCTION__);
+
+        const uint16_t payloadLength = this->payloadLength();
+        if (isResponse(EXIT_IDLE_CMD[2], 0x01) && payloadLength == 1 && buffer[5] == 0x00)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/*! @brief Exit manual IDLE mode. */
+bool Unit_UHF_RFID::exitIdle()
+{
+    sendCMD((uint8_t *)EXIT_IDLE_CMD, sizeof(EXIT_IDLE_CMD));
+    if (waitMsg())
+    {
+        debugFrame(__FUNCTION__);
+
+        const uint16_t payloadLength = this->payloadLength();
+        if (isResponse(EXIT_IDLE_CMD[2], 0x01) && payloadLength == 1 && buffer[5] == 0x00)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 uint8_t Unit_UHF_RFID::pollingOnce()
 {
     cleanCardsBuffer();
